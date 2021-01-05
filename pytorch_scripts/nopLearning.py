928fc699ab10b2a5bfb58fe4ca48002fee979394
Adam Prugel-Bennett <apb@ecs.soton.ac.uk>
2021-01-05 11:52:58 +0000

    Start Learning Framework

--- pytorch_scripts/nopLearning.py
+++ pytorch_scripts/nopLearning.py
@@ -1,73 +1,36 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
-import numpy as np
-import MultiMnistDataset
+import sys
+from datetime import datetime
+import time
+import argparse
+from tinydb import TinyDB, Query
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class NoPLearning:
+    def __init__(self, sys_argv=None):
+        if sys_argv is None:
+            sys_argv = sys.argv[1:]
+        description = "Train Naming of Parts Learner"
+        parser = argparse.ArgumentParser(prog="noLearning",
+                                         description=description)
+        parser.add_argument("-batch_size", metavar="N", type=int,
+                   help="Batch Size", default=6)
+        parser.add_argument("-learning_rate", metavar="N", type=float,
+                            help="Learning Rate", default=0.01)
+        self.my_args = parser.parse_args(sys_argv)
+        self.batch_size = self.my_args.batch_size
+        self.learning_rate = self.my_args.learning_rate
+        
+    def main(self):
+        data = {}
+        start_time = time.time();
+        timeStr = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+        data["start_time"] = timeStr
+        data["args"] = vars(self.my_args)
+        
+        
 
-train_dataset = MultiMnistDataset('../data/multiMNist/train/')
+        data["run_time"] = time.time()-start_time
+        db = TinyDB("../data/runs/"+type(self).__name__+"_data.json")
+        db.insert(data)
 
-train_loader = DataLoader(dataset, batch_size=5, shuffle=True)
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-        # Spatial transformer localization-network
-        self.localization = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=7),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True),
-            nn.Conv2d(8, 10, kernel_size=5),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True)
-        )
-
-        # Regressor for the 3 * 2 affine matrix
-        self.fc_loc = nn.Sequential(
-            nn.Linear(10 * 3 * 3, 32),
-            nn.ReLU(True),
-            nn.Linear(32, 3 * 2)
-        )
-
-        # Initialize the weights/bias with identity transformation
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
-
-    # Spatial transformer network forward function
-    def stn(self, x):
-        xs = self.localization(x)
-        xs = xs.view(-1, 10 * 3 * 3)
-        theta = self.fc_loc(xs)
-        theta = theta.view(-1, 2, 3)
-
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid)
-
-        return x
-
-    def forward(self, x):
-        # transform the input
-        x = self.stn(x)
-
-        # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-
-model = Net().to(device)
+if __name__ == "__main__":
+    NoPLearning().main()
